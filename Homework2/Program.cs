4022c445c660e473baebabd5a378bc5a7776bdc1
FlooGeR31 <fridrih.oushen@yandex.ru>
2023-07-25 10:38:31 +0300

    NewBranch

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -4,14 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 
+    (Если решаете математически через рандом, то проверка не нужна на трехзначность. Если через строки, то она обязательна)
+456-> 5
+782-> 8
+918-> 1
+*/
+
 namespace Homework2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.ReadKey();
+            int num = new Random().Next(100, 1000);
+            string NumStr = num.ToString();
+            if (NumStr.Length == 3)
+            {
+                Console.WriteLine($"Выпало число: {num}");
+                Console.WriteLine($" Число по середине: {NumStr[1]}");
+                Console.ReadKey();
+            }
         }
     }
 }
